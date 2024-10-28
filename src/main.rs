9c25f2cdf562e8c530fb0097521982b868f19a85
kostaskoukos <kkonst2014@gmail.com>
2024-10-28 15:59:38 +0200

    add rng functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to the guessing game! I will think of a number in the range [1, 100] and you'll try to guess it");
+
+    let secret = rand::thread_rng().gen_range(0..=100);
 }
